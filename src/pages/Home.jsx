9692367448f8e3bd76e50df557040df49dab6b79
Phan Cong Long <pcl2k2@gmail.com>
2022-03-25 00:30:18 +0700

    Add bannerslider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import BannerSlide from "../components/bannerSlide/BannerSlide";
 import Header from "../components/header/Header";
 
 function Home(props) {
   return (
     <div>
       <Header />
+      <BannerSlide />
     </div>
   );
 }
