231c561c6f6cd01ed274545044b2ed69d9dc5b35
Phan Cong Long <pcl2k2@gmail.com>
2022-03-30 21:51:14 +0700

    Fix Volumne issue in Player component

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -4,12 +4,14 @@ import "./Input.scss";
 
 const Input = (props) => {
   return (
-    <input
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange ? (e) => props.onChange(e) : null}
-    />
+    <div className="search-input">
+      <input
+        type={props.type}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange ? (e) => props.onChange(e) : null}
+      />
+    </div>
   );
 };
 
