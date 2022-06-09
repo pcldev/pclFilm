006fd00e6775209b88c02f97c6cb12422c142cbb
reddevill2k2 <pcl2k2@gmail.com>
2022-06-09 09:04:18 +0700

    Fix list film file

--- src/components/cardFilm/ListFilm.jsx
+++ src/components/cardFilm/ListFilm.jsx
@@ -10,11 +10,11 @@ function ListFilm(props) {
         ? items.map((item, index) => (
             <li key={index}>
               <CardFilm
-                imageUrl={item.coverVerticalUrl}
-                title={item.name}
-                type={item.dramaType.code}
-                id={item.id}
-                category={item.category}
+                imageUrl={item?.coverVerticalUrl}
+                title={item?.name}
+                type={item?.dramaType?.code}
+                id={item?.id}
+                category={item?.category}
                 domainType={item?.domainType}
               />
             </li>
